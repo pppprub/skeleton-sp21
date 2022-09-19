1016a97b1d169ec025ed0264d537e7c303173afd
pppprub <lizhangzhang123@gmail.com>
2022-09-19 01:04:25 -0500

    lab3 finished

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -22,4 +22,41 @@ public class TestBuggyAList {
             assertEquals(a.removeLast(),b.removeLast());
         }
     }
+    @Test
+    public void ramdomizedTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                B.addLast(randVal);
+                assertEquals(L.size(),B.size());
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size_L = L.size();
+                int size_B = B.size();
+                assertEquals(size_L,size_B);
+                System.out.println("sizeL: " + size_L + "sizeB: " + size_B);
+            }else  {
+                if (L.size() > 0){
+                    if(operationNumber == 2){
+                       int last_L =  L.getLast();
+                        int last_B =  B.getLast();
+                        assertEquals(last_L,last_B);
+                        System.out.println("LastL: " + last_L+"LastB: " + last_B);}
+                else if (operationNumber == 3){
+                            int rmv_last_L =  L.removeLast();
+                            int rmv_last_B =  B.removeLast();
+                            assertEquals(rmv_last_L,rmv_last_B);
+                            System.out.println("removeLast(L: " + rmv_last_L + "B: " + rmv_last_B + ")");
+                    }
+                }
+            }
+        }
+    }
 }
