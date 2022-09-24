0f3100c003d1591f6ccd1c32d0493d31e4a36ffb
pppprub <lizhangzhang123@gmail.com>
2022-09-23 23:16:32 -0500

    proj1 LinkedListDeque.java Part2 partial

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -37,6 +37,44 @@ public class ArrayDeque<T> {
         nextLast++;
         size++;
     }
+    public T remove(int index){
+        T remove_value = items[index];
+        items[index] = null;
+        size--;
+        return remove_value;
+    }
+
+    public T removeFirst(){
+        if (size > 0){
+            int index_Fast;
+            if(nextFirst != items.length){
+            index_Fast = nextLast + 1;
+            }else{
+                index_Fast = 0;
+            }
+            nextFirst = index_Fast;
+            return remove(index_Fast);
+        }else{
+            return null;
+        }
+
+    }
+
+    public T removeLast(){
+        if (size > 0){
+            int index_Last;
+            if(nextLast != 0){
+                index_Last = nextLast - 1;
+            }else{
+                index_Last = items.length;
+            }
+            nextLast = index_Last;
+            return remove(index_Last);
+        }else{
+            return null;
+        }
+
+    }
 
     public boolean IsFull(){
         if(items.length == size){
